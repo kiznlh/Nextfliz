dd6f954a02795ecba7506d3c34d831f9967222f1
Kizark <kiznlh@gmail.com>
2024-03-13 00:09:55 +0700

    structuring view and controls, will add later

---
+++ Views/MainApp/WindowUserMainWindow.xaml.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace Nextfliz.Views.MainApp
+{
+    /// <summary>
+    /// Interaction logic for WindowUserMainWindow.xaml
+    /// </summary>
+    public partial class WindowUserMainWindow : Window
+    {
+        public WindowUserMainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void Search_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+        private void Home_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+        private void User_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+    }
+}
