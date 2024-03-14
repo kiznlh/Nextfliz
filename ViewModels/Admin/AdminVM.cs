11e1e3a54d3a38cc408a71e7f0238b9f28de12b4
Thuận Phát <phat06112003@gmail.com>
2024-03-14 14:28:34 +0700

    Add admin view model

--- ViewModels/Admin/AdminVM.cs
+++ ViewModels/Admin/AdminVM.cs
@@ -1,37 +1,31 @@
-﻿using System;
+﻿using LiveCharts;
+using LiveCharts.Wpf;
+using Nextfliz.Views;
+using System;
 using System.Collections.Generic;
 using System.Collections.ObjectModel;
-using System.IO;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
-using System.Windows;
-using System.Windows.Controls;
-using System.Windows.Data;
-using System.Windows.Documents;
-using System.Windows.Input;
-using System.Windows.Media;
-using System.Windows.Media.Imaging;
-using System.Windows.Shapes;
 
-using LiveCharts;
-using LiveCharts.Wpf;
-
-namespace Nextfliz.Views
+namespace Nextfliz
 {
-    /// <summary>
-    /// Interaction logic for Admin.xaml
-    /// </summary>
-    public partial class Admin : Window
+    public class TopFilm
+    {
+        public string no { get; set; }
+        public string name { get; set; }
+        public string director { get; set; }
+        public string date { get; set; }
+        public string sell { get; set; }
+    }
+
+    class AdminVM
     {
         public SeriesCollection SeriesCollection { get; set; }
         public string[] Labels { get; set; }
         public Func<double, string> YFormatter { get; set; }
 
-        public Admin()
-        {
-            InitializeComponent();
-
+        public AdminVM() {
             ObservableCollection<TopFilm> list = new ObservableCollection<TopFilm>();
             list.Add(new TopFilm()
             {
@@ -56,14 +50,6 @@ namespace Nextfliz.Views
                 director = "Vũ",
                 date = "31-01-2023",
                 sell = "1000.001"
-            }); 
-            list.Add(new TopFilm()
-            {
-                no = "4",
-                name = "Chàng trai năm ấy",
-                director = "Vũ",
-                date = "31-01-2023",
-                sell = "1000.001"
             });
             list.Add(new TopFilm()
             {
@@ -73,15 +59,21 @@ namespace Nextfliz.Views
                 date = "31-01-2023",
                 sell = "1000.001"
             });
-
-            TopFilmDataGrid.ItemsSource = list;
+            list.Add(new TopFilm()
+            {
+                no = "4",
+                name = "Chàng trai năm ấy",
+                director = "Vũ",
+                date = "31-01-2023",
+                sell = "1000.001"
+            });
 
             SeriesCollection = new SeriesCollection
             {
                 new LineSeries
                 {
                     Title = "Doanh thu",
-                    Values = new ChartValues<double> { 432424, 60342424, 50234234, 2024230 ,4124242 }
+                    Values = new ChartValues<double> { 100432424, 60342424, 50234234, 2024230 ,4124242 }
                 },
                 new LineSeries
                 {
@@ -92,23 +84,6 @@ namespace Nextfliz.Views
 
             YFormatter = value => value.ToString("C");
             Labels = new[] { "Jan", "Feb", "Mar", "Apr", "May" };
-            DataContext = this;
-
-            SalesCombobox.SelectedIndex = 0;
         }
-
-        private void Button_Click(object sender, RoutedEventArgs e)
-        {
-
-        }
-    }
-
-    public class TopFilm
-    {
-        public string no { get; set; }
-        public string name { get; set; }
-        public string director { get; set; }
-        public string date { get; set; }
-        public string sell { get; set; }
     }
 }
---
+++ Views/Admin/Admin.xaml.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+using LiveCharts;
+using LiveCharts.Wpf;
+
+namespace Nextfliz.Views
+{
+    /// <summary>
+    /// Interaction logic for Admin.xaml
+    /// </summary>
+    public partial class Admin : Window
+    {
+        public SeriesCollection SeriesCollection { get; set; }
+        public string[] Labels { get; set; }
+        public Func<double, string> YFormatter { get; set; }
+
+        public Admin()
+        {
+            InitializeComponent();
+            DataContext = new AdminVM();
+        }
+
+    }
+}
